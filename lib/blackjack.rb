e4703cd3982e2ae4e90213f41fbd1829820011e4
Joshua Ticzon <ticzon.joshua@gmail.com>
2017-11-21 17:15:14 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -19,7 +19,8 @@ def get_user_input
 end
 
 def end_game
-  puts "Sorry you hit #{card_total} Thanks for playing!"
+  total = card_total
+  puts "Sorry you hit #{total} Thanks for playing!"
 end
 
 def initial_round
