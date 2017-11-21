1a3f3ff6e5e9e72d9f29b70cc1c2773cc420ff8d
Joshua Ticzon <ticzon.joshua@gmail.com>
2017-11-21 17:22:23 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -23,11 +23,9 @@ def end_game(card_total)
 end
 
 def initial_round(card_total)
-  card_total = 0
   deal_card
   deal_card
   display_card_total
-  puts "Your cards add up to #{card_total}"
 end
 
 def hit?
